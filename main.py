c98543deaa1ed9a10ebadeb91dc900e4ee02a9d8
Flavian16Vali <91555258+Flavian16Vali@users.noreply.github.com>
2021-10-13 11:47:02 +0300

    lab 3 terminat

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def get_longest_all_primes(lst: list[int]) -> list[int]:
 def test_get_longest_all_primes():
     assert get_longest_all_primes([1,2,3,4,5,6,7,8,9,10])==[2,3]
     assert get_longest_all_primes([2,3,5,7,11])==[2,3,5,7,11]
-    assert get_longest_all_primes([4,8,10])==[]
+    assert get_longest_all_primes([4,8,10])=="nu sunt numere prime"
 
 def main():
 
